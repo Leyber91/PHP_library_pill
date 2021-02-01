3354d2580b8ef6c69db4112ed7e351604b113f29
Leyber91 <leyber.91.2@gmail.com>
2021-02-01 08:40:42 +0100

    basic file structred completed

---
+++ strings.php
@@ -0,0 +1,9 @@
+<?php
+//We will be using dates from classes in this case from DateTime class forgetting the use of old php functions
+// ARITHMETIC OPERATORS
+echo '<h1> INFO </h1>';
+echo '<h2> We will use INFO function </h2>';
+
+echo '<p> phpinfo()</p>';
+phpinfo();
+?>
